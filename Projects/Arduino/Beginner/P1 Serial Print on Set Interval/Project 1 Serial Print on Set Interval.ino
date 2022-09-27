f3865056c7574cdf0202f89ee16848ae5e305cc3
Joshua Ball <joshua.ball@ttu.edu>
2022-09-26 21:21:18 -0600

    P1 complete

--- Projects/Arduino/Beginner/P1 Serial Print on Set Interval/Project 1 Serial Print on Set Interval.ino
+++ Projects/Arduino/Beginner/P1 Serial Print on Set Interval/Project 1 Serial Print on Set Interval.ino
@@ -1,5 +1,8 @@
-const long timeInterval = 2000; //The time interval at which we will print in mS.
-long previousTime = 0;
+const int timeInterval = 2000; //The time interval at which we will print in mS.
+int previousTime = 0; //This will hold the previous value of time once a loop iteration has
+                       // completed.
+
+
 void setup() {
   //This contains actions we will take prior to executing our main loop. In this case, we set
   //our baud rate to 9600 - standard.
@@ -8,16 +11,15 @@ void setup() {
 
 //Contains your main code to run repeatedly
 void loop() {
-  /* At the function loops, it will constantly be updating currentTime until
-   * the condition of our if statement is met. When it is met it will print
-   * our requested line and then set and save our current time to previous time.
-   * This allows us to keep track of how long we have been printing. 
+  /* At the function loops, it will constantly be updating currentTime 
+     until the condition of our if statement is met. When it is met it  
+     will print our requested line and then set and save our current time  
+     to previous time. This allows us to keep track of how long we have 
+     been printing. 
    */
-  unsigned long currentTime = millis();
-
+  unsigned int currentTime = millis();
   if(currentTime - previousTime >= timeInterval) {
     Serial.println("Hello World");
     previousTime = currentTime;
     }
-  
 }
